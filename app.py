e45853c453d7c6725d95c31b3e0ddc62051b6eef
Rafael-ORibeiro <92262160+Rafael-ORibeiro@users.noreply.github.com>
2021-11-25 23:06:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = "mongodb+srv://ror74559:12mm34@betcartoes.jbbi4.mongodb.net/campeonatos?retryWrites=true&w=majority"
+app.config['MONGO_URI'] = ""
 
 #collection produtos
 mongo = PyMongo(app)
